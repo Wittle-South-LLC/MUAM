0720c6cfb96f192e14ba6ddd12aee4880132b2a8
Eric Wittle <eric@wittle.net>
2019-06-30 13:23:52 -0400

    Setting version for python package

--- server/setup.py
+++ server/setup.py
@@ -4,7 +4,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name='muam',
-      version='0.4.0',
+      version='0.6.2',
       description='User and group management appliction',
       url='https://github.com/Wittle-South-LLC/MUAM',
       author='Wittle South Ventures, LLC',
